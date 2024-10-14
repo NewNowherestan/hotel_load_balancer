fa1f6f4060b5ef3f7a81aa55f725f20a2d194832
Stan Alexson <prods.by.stan@gmail.com>
2024-10-14 11:44:06 +0200

    init tests

---
+++ autostart/src/test/java/dev/stan/autostart/AutostartTest.java
@@ -0,0 +1,13 @@
+package dev.stan.autostart;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AutostartTest {
+    @Test
+    void testMain() {
+        boolean result = true;
+        assertThat(result).isTrue();
+    }
+
+}
